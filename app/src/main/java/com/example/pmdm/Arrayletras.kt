0c1f3e5c6a00a3013ef5239829011f736cb407d2
Carmen <cmarti@campuscamarafp.com>
2021-11-11 09:34:03 +0100

    hola

---
+++ app/src/main/java/com/example/pmdm/Arrayletras.kt
@@ -0,0 +1,11 @@
+package com.example.pmdm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Arrayletras : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_arrayletras)
+    }
+}
